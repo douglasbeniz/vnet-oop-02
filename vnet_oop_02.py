faed2ece9970626e7926bc8fe756dbef32bed71c
Douglas Bezerra Beniz <douglas.bezerra.beniz@ess.eu>
2021-07-12 16:58:11 +0200

    Adding examples of inheritance and extension;

--- vnet_oop_02.py
+++ vnet_oop_02.py
@@ -5,16 +5,47 @@ source https://runestone.academy/runestone/books/published/thinkcspy/Inheritance
 """
 
 class Point:
-
+    """ Class base """
     def __init__(self, initX, initY):
         self.x = initX
         self.y = initY
 
-    def distanceFromOrigin(self):
-        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
-
     def __str__(self):
         return "x=" + str(self.x) + ", y=" + str(self.y)
 
-p = Point(7, 6)
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+
+class LabeledPointInherit(Point):
+    """ Class inheriting from base one """
+    def __init__(self, initX, initY, label):
+        self.x = initX
+        self.y = initY
+        self.label = label
+
+    def __str__(self):
+        return "x=" + str(self.x) + ", y=" + str(self.y) + " (" + self.label + ")"
+
+
+class LabeledPointExtend(Point):
+    """ Class extending the base one """
+    def __init__(self, initX, initY, label):
+        super().__init__(initX, initY)
+        self.label = label
+
+    def __str__(self):
+        return super().__str__() + " (" + self.label + ")"
+
+
+# -----------------------------------------------------------------------------
+# Objects
+# -----------------------------------------------------------------------------
+p = Point(7.35, 6.81)
 print(p)
+
+labeledPt1 = LabeledPointInherit(4.9,3.2,"Here")
+print(labeledPt1)
+
+labeledPt2 = LabeledPointExtend(1.25,6.05,"There")
+print(labeledPt2)
